faf18a4e789c7986a3449152e4455cc6998f21c4
jrarkaan <rakajanitraa@hotmail.com>
2021-12-08 07:39:30 +0700

    raka

--- interface.go
+++ interface.go
@@ -1,11 +1,40 @@
-package main 
+package main
 
 import "fmt"
 
 type HasName interface {
-	
+	getName() string
+}
+
+func SayHello(hasName HasName){
+	fmt.Println("Hello", hasName.getName());
+}
+
+type Person struct {
+	Name string
+}
+
+func (person Person) getName() string{
+	return person.Name;
+}
+
+type Animal struct{
+	Name string
+}
+
+func (animal Animal) getName() string {
+	return animal.Name
 }
 
 func main(){
-	
+	var eko Person;
+	eko.Name = "Eko"
+
+	SayHello(eko);
+
+	cat := Animal {
+		Name: "Push",
+	}
+
+	SayHello(cat);
 }
