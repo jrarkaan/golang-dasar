64bfd440a8fa73eee303022ef8d260c8f1509302
jrarkaan <rakajanitraa@hotmail.com>
2021-12-11 09:37:40 +0700

    raka

--- pointer.go
+++ pointer.go
@@ -6,15 +6,34 @@ type address struct {
 	city, province, country string
 }
 
+func changeCountryIndonesia(address *address){
+	address.country = "Indonesia";
+}
+
 func main(){	
 
-	address1 := address{
+	var address1 address = address{
 		"Subang", "Jawa Barat", "Indonesia",
 	}
-	address2 := &address1;
+	var address2 *address = &address1; // pointer
 
 	address2.city = "Bandung";
 
+	// *address2 = address{"malang", "jawa timur", "indonesia"};
+
 	fmt.Println(address1);
 	fmt.Println(address2);
+
+	var address4 *address = new(address);
+	address4.city = "Jakarta";
+	fmt.Println(address4);
+
+	var alamat = address{
+		city: "subang",
+		province: "jawa barat",
+		country: "",
+	}
+	// var alamatPointer *address = &alamat
+	changeCountryIndonesia(&alamat);
+	fmt.Println(alamat)
 }
